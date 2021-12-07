48681e9d420838e7aec838f23f1638d5b8c24e8d
basusayantan2@gmail.com <basusayantan2@gmail.com>
2021-12-07 13:12:15 +0530

    first commit

--- App.js
+++ App.js
@@ -1,21 +1,114 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import {
+  FlatList,
+  SafeAreaView,
+  StyleSheet,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  View,
+  BackHandler,
+  Platform,
+  ActivityIndicator 
+} from 'react-native';
+import { WebView } from 'react-native-webview';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { visible: true };
+    BackHandler.addEventListener('hardwareBackPress', this.onBackPress);
+  }
+
+  hideSpinner=()=> {
+    this.setState({ visible: false });
+  }
+  showSpinner=()=> {
+    this.setState({ visible: true });
+  }
+  webView = {
+    canGoBack: false,
+    ref: null,
+  }
+
+  render() {
+    return (
+      <View style={{ flex: 1, }}>
+      <View style={{ position: 'relative', flexDirection: 'row', elevation: 8, padding: (0, 0, 0, 5), justifyContent: 'flex-start', backgroundColor: '#0c73ae', zIndex: 2,marginTop: 30 }}>
+                    <View style={{ flexDirection: 'column', paddingLeft: 10, marginTop: 5 }}>
+                        <Text style={{  fontSize: 16, alignItems: 'center', color: '#fff' }}>Dealer Portal</Text>
+                        <Text style={{ fontSize: 12, alignItems: 'center', color: '#fff' }}>DuraRoof</Text>
+                    </View>
+                </View>
+      <WebView
+        source={{ uri: 'http://52.172.218.225/expand/ERP/Duraroof$DuraRoof/p/account/login' }}
+        ref={(webView) => { this.webView.ref = webView; }}
+        onNavigationStateChange={(navState) => { this.webView.canGoBack = navState.canGoBack; }}
+        onLoadStart={() => this.showSpinner()}
+        onLoad={() => this.hideSpinner()}
+        javaScriptEnabled={true}
+          //For the Cache
+          domStorageEnabled={true}
+      />
+      {this.state.visible && (
+          <ActivityIndicator
+            style={styles.activityIndicatorStyle }
+            size="large"
+            color="#0000ff"
+          />
+        )}
+      </View>
+    );
+  }
+
+  onBackPress = () => {
+    if (this.webView.canGoBack && this.webView.ref) {
+      this.webView.ref.goBack();
+      return true;
+    }
+    return false;
+  }
+
+  // componentWillMount() {
+  //     BackHandler.addEventListener('hardwareBackPress', this.onBackPress);
+  // }
+
+  componentWillUnmount() {
+      BackHandler.removeEventListener('hardwareBackPress');
+  }
 }
+
+
+handleWebViewNavigationStateChange = (newNavState) => {
+  const { url } = newNavState;
+  if (!url) return;
+
+  // one way to handle a successful form submit is via query strings
+  if (!url.includes('http://52.172.218.225/')) {
+    this.webview.stopLoading();
+    WebBrowser.openBrowserAsync(url);
+    // maybe close this view?
+  }
+};
 
 const styles = StyleSheet.create({
   container: {
+    backgroundColor: '#F5FCFF',
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
+  },
+  activityIndicatorStyle: {
+    flex: 1,
+    position: 'absolute',
+    marginLeft: 'auto',
+    marginRight: 'auto',
+    marginTop: 'auto',
+    marginBottom: 'auto',
+    left: 0,
+    right: 0,
+    top: 0,
+    bottom: 0,
     justifyContent: 'center',
   },
 });
+
+export default App;
